16cfad8a30988d26bbf72f205c7b1a690640799d
Heltta <50683241+Heltta@users.noreply.github.com>
2022-07-14 21:21:32 -0300

    apagon

--- src/containers/ItemListContainer.js
+++ src/containers/ItemListContainer.js
@@ -1,44 +1,52 @@
-import React, {Component} from "react"
+import React, {useEffect, useState} from "react"
 import '../css/ItemListContainer.css'
 import ItemCount from './ItemCount.js'
 
-class ItemListContainer extends Component{
-    constructor(){
-        super();
-        this.state ={
-            products:[
-                {name:'Catan', price: 40, stock:4},
-                {name:'Secret Hitler', price: 25, stock:2},
-                {name:'Flick emm up', price: 20, stock:1},
-                {name:'Murder in HongKong', price: 16, stock:3}
-            ]
+function ItemListContainer(props) {
+    const [boardGames, setBoardGames] = useState([]);
+
+    async function getBoardGames(){
+        try{
+            // const dataParse = await fetch('../assets/json/tabletopGames.json')
+            await fetch('../assets/json/data.json')
+                .then((resp)=> resp.json())
+                .then((dataParse) => setBoardGames(dataParse));
+            // const dataParse = await resp.json();
+            // setBoardGames(dataParse);
+
+        }catch(error){
+            console.log(error);
         };
-    }
-    render (){
-        const items = this.state.products.map( prod =>(
-            <li className="producto">
-                <div>
-                    <span>{prod.name}</span>
-                    <span>{prod.price} dolares</span>
-                </div>
-                <div>{prod.name} imagen</div>
-                <ItemCount  stock={prod.stock} 
-                            initial={1}
-                            onAdd={(amount) => {
-                                console.log(`Se agregaron '${prod.name}' X${amount} al carrito`)
-                            }}/>
-            </li>
-        ))
-        return(
-            <div className="itemListContainer">
-                <h1>{this.props.saludo} a al sitio de {this.props.website}</h1>
-                <div id="breeadcrumbs">Breadcrumb: father/children/childrenOfChildren</div>
-                <section>
-                    <ul className="productList">{items}</ul>
-                </section>
-            </div>
-        );
     };
+    
+    useEffect(()=>{
+        getBoardGames();
+    },[]);
+
+    const items = boardGames.map( prod =>(
+        <li className="producto">
+            <div>
+                <span>{prod.name}</span>
+                <span>{prod.price} dolares</span>
+            </div>
+            <div>{prod.name} imagen</div>
+            <ItemCount  stock={prod.stock} 
+                        initial={1}
+                        onAdd={(amount) => {
+                            console.log(`Se agregaron '${prod.name}' X${amount} al carrito`)
+                        }}/>
+        </li>
+    ))
+
+    return(
+        <div className="itemListContainer">
+            <h1>{props.saludo} a al sitio de {props.website}</h1>
+            <div id="breeadcrumbs">Breadcrumb: father/children/childrenOfChildren</div>
+            <section>
+                <ul className="productList">{items}</ul>
+            </section>
+        </div>
+    );
 }
 
 export default ItemListContainer
