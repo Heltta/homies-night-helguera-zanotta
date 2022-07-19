c60e7a26719a7d8b776e894af6f569ce07e9fb90
Heltta <lucashelgueraz@gmail.com>
2022-07-19 19:57:24 -0300

    Update ItemDetailContainer.js
    Add fetch to getItem

--- src/containers/ItemDetailContainer.js
+++ src/containers/ItemDetailContainer.js
@@ -2,12 +2,30 @@ import React from "react";
 import ItemDetail from "../components/ItemDetail";
 
 const getItem = () =>{
-    <></>
-}
+    try{
+        // const dataParse = await fetch('../assets/json/tabletopGames.json')
+        setTimeout(
+            ()=>fetch('./resources/tabletopGames.json')
+                .then((resp)=> resp.json())
+                .then((dataParse) => {
+                    setBoardGames(dataParse);
+                    console.log(dataParse)
+            }),
+            2000
+        )
+        // const dataParse = await resp.json();
+        // setBoardGames(dataParse);
+
+    }catch(error){
+        console.log(error);
+    };
+
+};
 
 function ItemDetailContainer(){
     return(
         <>
+            <ItemDetail />
         </>
     )
 }
