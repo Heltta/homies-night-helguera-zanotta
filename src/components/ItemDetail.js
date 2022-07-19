1bc58acc80d1c5a70a55044269eeb43c12d2b8a0
Heltta <lucashelgueraz@gmail.com>
2022-07-19 19:11:45 -0300

    Add components template

---
+++ src/components/ItemDetail.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function ItemDetail({item}){
+    return(
+        <>
+        </>
+    );
+};
+
+export default ItemDetail
---
+++ src/containers/ItemDetailContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ItemDetail from "../components/ItemDetail";
+
+const getItem = () =>{
+    <></>
+}
+
+function ItemDetailContainer(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default ItemDetailContainer
