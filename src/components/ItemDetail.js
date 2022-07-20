baa63dfeb753ea44d499def812500ff10979efbe
Heltta <lucashelgueraz@gmail.com>
2022-07-19 22:45:50 -0300

    Fix ItemDetail image

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -4,9 +4,9 @@ function ItemDetail({item}){
     return(
         <section className="itemDetail">
             <div>{item?.name}</div>
-            <div>{item?.price}</div>
+            <div>{item?.price} dolares</div>
             <div>{item?.description}</div>
-            <div>{item?.photo}</div>
+            <img src={item?.photo} alt="tapa del juego"></img>
         </section>
     );
 };
