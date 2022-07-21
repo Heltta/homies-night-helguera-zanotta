fb73ae232dd33617ca3900aea6c9bfd19d93db6d
Heltta <lucashelgueraz@gmail.com>
2022-07-21 17:13:26 -0300

    Import react-router-dom to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar'
 import ItemDetailContainer from './containers/ItemDetailContainer';
 import ItemListContainer from './containers/ItemListContainer'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
