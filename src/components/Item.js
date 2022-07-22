dc619941d8bd08d06190ea7e0360a5c593c715fc
Heltta <lucashelgueraz@gmail.com>
2022-07-21 23:38:03 -0300

    Revert "Config item as link to detail"
    This reverts commit 9a15097bc365f3ea60412ce0a7b5504d95e47d84.

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,20 +5,19 @@ import { Link } from "react-router-dom"
 
 function Item({id, name, price, stock, photo  }){
     return(
-        <Link to={`/item/${id}`}>
-            <li id={id} className="producto">
-                <div>
-                    <span>{name}</span>
-                    <span>{price} dolares</span>
-                </div>
-                <img src={photo} alt={`caja de ${name}`}/>
-                <ItemCount  stock={stock} 
-                            initial={1}
-                            onAdd={(amount) => {
-                                console.log(`Se agregaron '${name}' X${amount} al carrito`)
-                            }}/>
-            </li>
-        </Link>
+        <li id={id} className="producto">
+            <div>
+                <span>{name}</span>
+                <span>{price} dolares</span>
+            </div>
+            <img src={photo} alt={`caja de ${name}`}/>
+            <ItemCount  stock={stock} 
+                        initial={1}
+                        onAdd={(amount) => {
+                            console.log(`Se agregaron '${name}' X${amount} al carrito`)
+                        }}/>
+            <Link to={`/item/${id}`}>Detalle</Link>
+        </li>
     )
 };
 
