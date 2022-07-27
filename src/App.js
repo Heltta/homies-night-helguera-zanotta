5b368973817b53299aa9f4bbff7459d883b70c38
Heltta <lucashelgueraz@gmail.com>
2022-07-27 20:40:39 -0300

    Wrap app into CartContext

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from './components/NavBar'
 import ItemDetailContainer from './containers/ItemDetailContainer';
 import ItemListContainer from './containers/ItemListContainer'
 import Cart from './containers/Cart'
+import CartContext from './context/CartContext';
 
 //Libraries
 
@@ -13,15 +14,17 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes >
-        <Route index path='/' element={<ItemListContainer saludo='Buenas noches y bienvenido' website='Homies Night'/>}/>
-        <Route index path='/category/:id' element={<ItemListContainer saludo='Buenas noches y bienvenido' website='Homies Night'/>}/>
-        <Route exact path='/item/:itemId' element={<ItemDetailContainer />}/>
-        <Route exact path='/cart' element={<Cart />}/>
-      </Routes>
-    </BrowserRouter>
+    <CartContext.Provider value={[]}>
+      <BrowserRouter>
+        <NavBar />
+        <Routes >
+          <Route index path='/' element={<ItemListContainer saludo='Buenas noches y bienvenido' website='Homies Night'/>}/>
+          <Route index path='/category/:id' element={<ItemListContainer saludo='Buenas noches y bienvenido' website='Homies Night'/>}/>
+          <Route exact path='/item/:itemId' element={<ItemDetailContainer />}/>
+          <Route exact path='/cart' element={<Cart />}/>
+        </Routes>
+      </BrowserRouter>
+    </CartContext.Provider>
   );
 }
 
