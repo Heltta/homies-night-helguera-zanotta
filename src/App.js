93eb6a19c3f77585775b1d5d551638e9bfedc2f7
Heltta <lucashelgueraz@gmail.com>
2022-07-27 20:39:51 -0300

    Tidy up App.js imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+//Stylesheets
 import './App.css';
+
+//Components
 import NavBar from './components/NavBar'
 import ItemDetailContainer from './containers/ItemDetailContainer';
 import ItemListContainer from './containers/ItemListContainer'
 import Cart from './containers/Cart'
+
+//Libraries
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
