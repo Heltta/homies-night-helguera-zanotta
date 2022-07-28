699c46d3271fdd0a0d9478e19079c41a8856dd83
Heltta <lucashelgueraz@gmail.com>
2022-07-28 20:24:58 -0300

    Stylize CartContainer

--- src/containers/CartContainer.js
+++ src/containers/CartContainer.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useCartContext } from "../context/CartContext";
+import '../css/CartContainer.css'
 
 function CartContainer(){
     const { cartList, clearCart, isCartEmpty, removeItem } = useCartContext();
@@ -7,20 +8,23 @@ function CartContainer(){
     if(isCartEmpty()){return(<h2>Su carrito se encuentra vacío</h2>)};
 
     return(
-            <>
+            <section className="cartList">
                 <ul>
                     {cartList.map(prod => 
                         <li key={prod.id}>
-                            <div>{prod.name}</div>
-                            <div> cantidad: {prod.quantity}</div>
+                            <h2 className="itemName">{prod.name}</h2>
                             <img src={prod.photo} alt="gamebox"/>
-                            <button onClick={()=> removeItem(prod.id)}>
-                                Eliminar {prod.name} del carrito
-                            </button>
+                            <span className="itemPrice"> Precio: ${prod.quantity}</span>
+                            <div className="cartItemUtility">
+                                <span className="itemQuantity"> Cantidad: {prod.quantity}</span>
+                                <button className="removeItem" onClick={()=> removeItem(prod.id)}>
+                                    Eliminar {prod.name} del carrito
+                                </button>
+                            </div>
                         </li>)}
                 </ul>
                 <button onClick={clearCart}>Limpiar carrito</button>
-            </>
+            </section>
         );
 }
 
