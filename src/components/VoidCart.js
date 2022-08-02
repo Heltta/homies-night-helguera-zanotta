4159ad08de74cdd0c53e6e3b5afc38c9cda7d0ef
Heltta <lucashelgueraz@gmail.com>
2022-08-02 17:45:44 -0300

    Add Link to landing from VoidCart.js

--- src/components/VoidCart.js
+++ src/components/VoidCart.js
@@ -1,7 +1,13 @@
+import {Link} from 'react-router-dom'
 
 function VoidCart(){
     return(
-        <h2>Su carrito se encuentra vacío</h2>
+        <>
+            <h2>Su carrito se encuentra vacío...</h2>
+            <Link to="/">
+                <span>Arreglemos eso!</span>
+            </Link>
+        </>
     )
 }
 
