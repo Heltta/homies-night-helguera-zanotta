24ae84930014b66ead4384f3a66591f559b1ab8a
Heltta <lucashelgueraz@gmail.com>
2022-08-09 18:01:07 -0300

    Add item quantity data to order

--- src/components/CartMakeOrder.js
+++ src/components/CartMakeOrder.js
@@ -9,18 +9,28 @@ function CartMakeOrder(){
     } = useCartContext();
 
     const buildOrderList = () =>{
+
         const buyer = {
             name:"Generic User",
             phone:"+012345678",
             email:"place_holder@mail.com",
         }
+
         const items = [];
         cartList.forEach(item => {
-            items.push({id:item.id, title:item.name, price:item.price})
+            items.push({
+                id:item.id,
+                title:item.name,
+                price:item.price,
+                quantity:item.quantity,
+            })
         });
+
         const order = {buyer, items, total:cartCost()};
+
         console.log(order);
         sendOrder(order);
+
         return
     }
     return(
