0d72aa44d4f933b72739fcdc2be35932ca67265e
Heltta <lucashelgueraz@gmail.com>
2022-08-13 22:26:13 -0300

    Implement Loading for ItemListContainer

--- src/containers/ItemListContainer.js
+++ src/containers/ItemListContainer.js
@@ -3,23 +3,38 @@ import '../css/ItemListContainer.css'
 import ItemList from './ItemList.js'
 import { getCollection } from "../helpers/getProducts";
 import {useParams} from "react-router-dom"
+import Loading from "../components/Loading"
 
 function ItemListContainer(props) {
     const [boardGames, setBoardGames] = useState([]);
+    const [ loading, setLoading ] = useState(true);
     const { id } = useParams();
 
     useEffect(()=>{
-        getCollection((fbData)=> setBoardGames(fbData), id);
+        //Load items and change loading state
+        //to false when loaded.
+        getCollection(
+            (fbData)=> {
+                setBoardGames(fbData);
+                setLoading(false);
+            },
+            id);
+        //Reset loading state to true when category changes
+        return(setLoading(true))
     },[id])
 
+    
     return(
-        <div className="itemListContainer">
-            <h1>{props.saludo} a al sitio de {props.website}</h1>
-            <div id="breeadcrumbs">Breadcrumb: father/children/childrenOfChildren</div>
-            <section>
-                <ItemList products={boardGames} />
-            </section>
-        </div>
+        (loading)? 
+            <Loading />
+            : 
+            <div className="itemListContainer">
+                <h1>{props.saludo} a al sitio de {props.website}</h1>
+                <div id="breeadcrumbs">Breadcrumb: father/children/childrenOfChildren</div>
+                <section>
+                    <ItemList products={boardGames} />
+                </section>
+            </div>
     );
 }
 
