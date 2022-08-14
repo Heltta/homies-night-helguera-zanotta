f5307030e6eddf97eb550b2a95cd38469b1ab098
Heltta <lucashelgueraz@gmail.com>
2022-08-13 23:38:55 -0300

    Implement Loading for ItemDetailContainer

--- src/containers/ItemDetailContainer.js
+++ src/containers/ItemDetailContainer.js
@@ -2,20 +2,34 @@ import React, { useEffect, useState } from "react";
 import ItemDetail from "../components/ItemDetail";
 import { getItem } from "../helpers/getProducts";
 import {useParams} from "react-router-dom"
+import Loading from "../components/Loading"
 
 function ItemDetailContainer(){
     const [item, setItem] = useState({});
+    const [ loading, setLoading ] = useState(true);
     const { itemId } = useParams();
 
     useEffect(()=>{
-        getItem((fbData)=> setItem(fbData), itemId);
+        //Load item and change loading state
+        //to false when loaded.
+        getItem(
+            (fbData)=> {
+                setItem(fbData);
+                setLoading(false);
+            },
+            itemId);
+            //Reset loading state to true when category changes
+            return(setLoading(true))
     },
     [itemId])
 
     return(
-        <section className="itemDetailContainer">
-            <ItemDetail item={item}/>
-        </section>
+        (loading)? 
+            <Loading />
+            : 
+            <section className="itemDetailContainer">
+                <ItemDetail item={item}/>
+            </section>
     )
 }
 
