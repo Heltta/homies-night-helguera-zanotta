dc78a7f322fd1b2fef0657f48f70935958b8b084
Heltta <lucashelgueraz@gmail.com>
2022-08-20 17:43:14 -0300

    Refactor MyOrder form
    Create new component for MyOrder formulary.

---
+++ src/components/MyOrderForm.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import '../css/OrderForm.css'
+
+import getOrder from "../helpers/getOrder";
+
+function MyOrderForm({ setState }){
+    const [orderId, setOrderId] = useState('');
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        if(orderId){
+            getOrder(
+                (fbData)=> {
+                    setState(fbData);
+                },
+                orderId);
+        }
+        else{
+            (orderId === '' )? 
+                console.log('You must write an id')
+                :
+                console.log(`${orderId} is not a valid id`)
+                ;
+        }
+    }
+
+    return(
+        <form
+            onSubmit={handleSubmit}>
+            <label>
+                Identificador de orden
+                <input type="text" 
+                    value={orderId} 
+                    onChange={(event) => setOrderId(event.target.value)} />
+            </label>
+
+            <input type="submit" value="Submit" />
+        </form>
+
+    )
+}
+
+export default MyOrderForm
--- src/containers/MyOrderContainer.js
+++ src/containers/MyOrderContainer.js
@@ -1,46 +1,20 @@
 import { useEffect, useState } from "react";
-import '../css/OrderForm.css'
 
-import getOrder from "../helpers/getOrder";
+import MyOrderForm from "../components/MyOrderForm";
 
 function MyOrderContainer(){
-    const [orderId, setOrderId] = useState('');
     const [order, setOrder] = useState();
 
     useEffect(()=>{
+        //Placeholder useEffect function
         console.log(`Mount or "order" change`);
         console.log(order);
     },[order]);
     
-    return(
-        <form onSubmit={
-            (e)=>{
-                e.preventDefault();
-                if(orderId){
-                    getOrder(
-                        (fbData)=> {
-                            setOrder(fbData);
-                        },
-                        orderId);
-                }
-                else{
-                    (orderId === '' )? 
-                        console.log('You must write an id')
-                        :
-                        console.log(`${orderId} is not a valid id`)
-                        ;
-                }
-            }
-        }>
-            <label>
-                Identificador de orden
-                <input type="text" 
-                    value={orderId} 
-                    onChange={(event) => setOrderId(event.target.value)} />
-            </label>
-
-            <input type="submit" value="Submit" />
-        </form>
+    return( 
+        <>
+            <MyOrderForm setState={setOrder}/>
+        </>
     );
 }
 
