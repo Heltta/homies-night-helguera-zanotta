93ec5c5d000a8557d8a926dd4c0837a85b6001ca
Heltta <lucashelgueraz@gmail.com>
2022-08-24 13:12:59 -0300

    Add Grid to items
    Add grid to items inside ItemListContainer

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,13 +4,12 @@ import { Link } from "react-router-dom"
 
 function Item({id, name, price, stock, photo  }){
     return(
-        <li id={id} className="producto">
-            <div>
-                <span>{name}</span>
-                <span>{price} dolares</span>
-            </div>
-            <img src={photo} alt={`caja de ${name}`}/>
-            <Link to={`/item/${id}`}><button>Detalle</button></Link>
+        <li id={id} className="product">
+            <div className="name">{name}</div>
+            <div className="price">$ {price}</div>
+            <div className="stock">Quedan {stock}</div>
+            <img className="photo" src={photo} alt={`caja de ${name}`}/>
+            <Link className="detail" to={`/item/${id}`}><button>Detalle</button></Link>
         </li>
     )
 };
