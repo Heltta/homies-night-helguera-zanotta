ea0b983420feb7c69e2fc603c97bb53fb7c885a9
Heltta <lucashelgueraz@gmail.com>
2022-08-24 20:27:50 -0300

    Remove deprecated AccountWidget Code

--- src/components/AccountWidget.js
+++ src/components/AccountWidget.js
@@ -1,22 +1,7 @@
-import { useEffect, useState, useCallback } from 'react'
 import profile from '../assets/images/profile.svg'
 import { Link } from 'react-router-dom'
 
 function AccountWidget(){
-    const [openMenu, setOpenMenu] = useState(false);
-    
-    const  cerrar = useCallback( ()=> {
-        setOpenMenu(false)
-    },[]);
-
-    useEffect(()=> {
-        if(openMenu){
-            window.addEventListener('click', cerrar)
-        }
-        else{
-            window.removeEventListener('click', cerrar);
-        }
-    },[openMenu, cerrar])
 
     return(
         <>
